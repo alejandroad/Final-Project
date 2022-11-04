db2f348ea91f343909f810b72fbe49f91c19995a
akrambettayeb <bettayeb.akram@gmail.com>
2022-11-04 16:22:14 -0500

    added activity switch intent button to camera activity

--- app/src/main/java/com/example/lab4/MainActivity.kt
+++ app/src/main/java/com/example/lab4/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.lab4
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.lab4.databinding.ActivityMainBinding
@@ -7,6 +8,7 @@ import com.example.lab4.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var buttonIntent: Intent
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,5 +16,19 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.cameraBtn.setOnClickListener() { launchCameraActivity() }
+        binding.profileBtn.setOnClickListener() { launchProfileActivity() }
+
+    }
+
+    private fun launchProfileActivity() {
+        buttonIntent = Intent(this, CameraActivity::class.java)
+        startActivity(buttonIntent)
+    }
+
+    private fun launchCameraActivity() {
+        buttonIntent = Intent(this, CameraActivity::class.java)
+        startActivity(buttonIntent)
     }
 }
