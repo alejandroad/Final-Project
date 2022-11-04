d7b13b27fe6a7fae64fa5017f6685a376893b2a5
akrambettayeb <bettayeb.akram@gmail.com>
2022-11-04 11:38:13 -0500

    Revert "Merge pull request #6 from alejandroad/aleBranch"
    This reverts commit fb74fc56961ad0f1231f1deae748c1ea793443ca, reversing
    changes made to 1c634f0a19f134a0bfc4cbf6c89bea3ee8fc0dae.

--- app/src/main/java/com/example/lab4/ProfileActivity.kt
+++ app/src/main/java/com/example/lab4/ProfileActivity.kt
@@ -1,57 +1,11 @@
 package com.example.lab4
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.app.ActionBar
-import com.example.lab4.databinding.ActivityProfileBinding
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 
 class ProfileActivity : AppCompatActivity() {
-
-    // ViewBinding
-    private lateinit var binding: ActivityProfileBinding
-
-    // Action Bar
-    private lateinit var actionBar: ActionBar
-
-    // Firebase
-    private lateinit var auth: FirebaseAuth
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityProfileBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        // config action bar
-        actionBar = supportActionBar!!
-        actionBar.title = "Profile"
-
-        // init firebase
-        auth = Firebase.auth
-        checkUser()
-
-        // logout handler
-        binding.logoutBtn.setOnClickListener {
-            auth.signOut()
-            checkUser()
-        }
-    }
-
-    private fun checkUser() {
-        val user = auth.currentUser
-
-        if(user != null) {
-            // user is logged in
-            val email = user.email
-            binding.welcomeTV.text = email
-        } else {
-            // user is not logged in
-            startActivity(Intent(this, LogInActivity::class.java))
-            finish()
-        }
+        setContentView(R.layout.activity_profile)
     }
 }
