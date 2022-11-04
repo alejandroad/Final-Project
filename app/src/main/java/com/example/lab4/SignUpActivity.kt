34232c03136be969ec5fbf31934aec53dfdd6f31
Alejandro Arias <alejandroarias@wireless-10-145-41-101.public.utexas.edu>
2022-11-04 12:36:46 -0500

    readded firebase

--- app/src/main/java/com/example/lab4/SignUpActivity.kt
+++ app/src/main/java/com/example/lab4/SignUpActivity.kt
@@ -1,11 +1,113 @@
 package com.example.lab4
 
+import android.app.ProgressDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.util.Patterns
+import android.widget.Toast
+import androidx.appcompat.app.ActionBar
+import com.example.lab4.databinding.ActivityLogInBinding
+import com.example.lab4.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SignUpActivity : AppCompatActivity() {
+    // ViewBinding
+    private lateinit var binding: ActivitySignUpBinding
+
+    // Action Bar
+    private lateinit var actionBar: ActionBar
+
+    // Progress Dialog
+    private lateinit var progressDialog: ProgressDialog
+
+    // Firebase Authentication
+    private lateinit var auth: FirebaseAuth
+
+    // Data
+    private var email = ""
+    private var password = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Configure ActionBar
+        actionBar = supportActionBar!!
+        actionBar.title = "Sign up"
+        // enable the back button
+        actionBar.setDisplayHomeAsUpEnabled(true)
+        actionBar.setDisplayShowHomeEnabled(true)
+
+        // configure progress dialog
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("One moment...")
+        progressDialog.setMessage("Creating account")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        // init firebase
+        auth = Firebase.auth
+
+        // onclick
+        binding.SignUpButton.setOnClickListener {
+            // validate data
+            validateData()
+        }
+    }
+
+    private fun validateData() {
+        email = binding.emailA.text.toString().trim()
+        password = binding.PasswordA.text.toString().trim()
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            // incorrect email semantics
+            binding.emailA.error = "Incorrect email semantics"
+        } else if (TextUtils.isEmpty(password)) {
+            // the user did not input a password
+            binding.PasswordA.error = "No password imputed"
+        } else if (password.length < 8) {
+            // password length is less than 8
+            binding.PasswordA.error = "Password must be at least 8 characters long"
+        } else {
+            // password validated
+            // TODO add more password checks
+            createAccount()
+        }
+    }
+
+    private fun createAccount() {
+        // show progress
+        progressDialog.show()
+
+        // create account
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this) {task ->
+
+            if(task.isSuccessful) {
+                // user created account successfully
+                progressDialog.dismiss()
+                val user = auth.currentUser
+                val email = user!!.email
+                Toast.makeText(this, "Account created with $email", Toast.LENGTH_SHORT).show()
+
+                startActivity(Intent(this, ProfileActivity::class.java))
+                finish()
+
+            } else {
+                // user did not create account successfully
+                progressDialog.dismiss()
+                Log.d("ERROR_MSG", "user did not create account successfully")
+                Toast.makeText(this, "Sign up failed due to ${task.exception}", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed() // go back to previous activity
+        return super.onSupportNavigateUp()
     }
 }
